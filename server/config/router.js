883d88f2431c74a92a0552001410f9a88abc0cc1
raul.dasgupta <raul.dasgupta@vocus.com.au>
2018-06-25 15:54:13 +0800

    updated router.js so all api calls require user validation

--- server/config/router.js
+++ server/config/router.js
@@ -15,17 +15,17 @@ module.exports = function(app){
         return res.send(res.result)
     }
     //CRUD Routes for maps
-    app.get('/api/maps',controllers.MapController.getMaps,routeLogger)
-    app.get('/api/maps/:id',controllers.MapController.getMapsById,routeLogger)
-    app.post('/api/maps',controllers.MapController.createNewMap,routeLogger)
-    app.put('/api/maps/:id',controllers.MapController.updateMap,routeLogger)
-    app.delete('/api/maps/:id',controllers.MapController.deleteMap,routeLogger)
+    app.get('/api/maps',injectors.setUserFromCookie,controllers.MapController.getMaps,routeLogger)
+    app.get('/api/maps/:id',injectors.setUserFromCookie,controllers.MapController.getMapsById,routeLogger)
+    app.post('/api/maps',injectors.setUserFromCookie,controllers.MapController.createNewMap,routeLogger)
+    app.put('/api/maps/:id',injectors.setUserFromCookie,controllers.MapController.updateMap,routeLogger)
+    app.delete('/api/maps/:id',injectors.setUserFromCookie,controllers.MapController.deleteMap,routeLogger)
     // Crud Routes for users
-    app.get('/api/users',controllers.UserController.getUsers,routeLogger)
-    app.get('/api/users/:id',controllers.UserController.getUsersById,routeLogger)
-    app.post('/api/users',controllers.UserController.createNewUser,routeLogger)
-    app.put('/api/users/:id',controllers.UserController.updateUser,routeLogger)
-    app.delete('/api/users/:id',controllers.UserController.deleteUser,routeLogger)
+    app.get('/api/users',injectors.setUserFromCookie,controllers.UserController.getUsers,routeLogger)
+    app.get('/api/users/:id',injectors.setUserFromCookie,controllers.UserController.getUsersById,routeLogger)
+    app.post('/api/users',injectors.setUserFromCookie,controllers.UserController.createNewUser,routeLogger)
+    app.put('/api/users/:id',injectors.setUserFromCookie,controllers.UserController.updateUser,routeLogger)
+    app.delete('/api/users/:id',injectors.setUserFromCookie,controllers.UserController.deleteUser,routeLogger)
 
     // Routes for the application
     app.get('/', injectors.setUserFromCookie,controllers.ViewController.renderLogin,routeLogger)
