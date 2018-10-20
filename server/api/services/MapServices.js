cabb8a1710f9e425ffb6a7cc1ade7b14d14df3b0
Rahul <k.dasgupta112@gmail.com>
2018-10-20 22:22:43 +0800

    removed function to retrieve map service

--- server/api/services/MapServices.js
+++ server/api/services/MapServices.js
@@ -1,21 +1,10 @@
-const request = require('request');
+
 class MapServices extends base {
   constructor(model) {
     super(model);
     this.model = model;
   }
 
-  fetchMapData(source,options){
-      return new Promise((resolve,reject)=>{
-          request.get(source,(error,resp,body)=>{
-              if(error || resp.statusCode!==200){
-                  if(error)reject(error)
-                  reject(new Error('Bad Request'))
-              }
-              resolve(JSON.parse(body))
-          })
-      })
-  }
 }
 
 
